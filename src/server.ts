86d48628bb59cafe4811bd1c633a68d436aba0d5
Bruno Petrolini <brunopetrolini@hotmail.com>
2025-03-19 20:35:33 -0300

    feat: adds server initialization

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,24 @@
-console.log('Hello World');
+import fastify from 'fastify';
+import mongoose from 'mongoose';
+
+const app = fastify({
+  logger: true,
+});
+
+const initServer = async () => {
+  try {
+    const port = Number(process.env.PORT) || 3000;
+    await app.listen({ port });
+  } catch (err) {
+    app.log.error(err);
+    process.exit(2);
+  }
+};
+
+mongoose
+  .connect('mongodb://docker:docker@localhost:27017/waiter?authSource=admin')
+  .then(() => initServer())
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });

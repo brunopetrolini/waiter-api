d56d91a72c258fa951948bfc4798596f35f2f36f
Bruno Petrolini <brunopetrolini@hotmail.com>
2025-03-20 22:12:23 -0300

    feat: serves static images from uploads folder

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
+import path from 'node:path';
+
 import express from 'express';
 import mongoose from 'mongoose';
 
@@ -6,6 +8,7 @@ import { router } from './router';
 const app = express();
 
 app.use(express.json());
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(router);
 
 const initServer = () => {

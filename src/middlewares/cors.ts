84a361950719d7e0cf067afd451f3e51ec174755
Bruno Petrolini <brunopetrolini@hotmail.com>
2025-04-11 20:15:31 -0300

    feat: implements cors middleware

---
+++ src/middlewares/cors.ts
@@ -0,0 +1,7 @@
+import { NextFunction, Request, Response } from 'express';
+
+export function cors(request: Request, response: Response, next: NextFunction) {
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Allow-Methods', '*');
+  next();
+}
